b90d2a7cb8073c240a25db8d93ec48c217180f87
Mohit kumar <mk513147@gmail.com>
2025-03-28 10:35:29 +0530

    fix: correct MongoDB connection string syntax

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js"
 
 const connectDB = async () => {
     try {
-        const response = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}}`);
+        const response = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`Connection established!! \n DB host: ${response.connection.host}`);
 
 
